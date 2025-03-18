51abc36de56da8676eccff79144adf68efa0a539
diogo <diogo.lima.flooxai@gmail.com>
2025-03-18 10:54:55 -0300

    feat: add method for

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -60,11 +60,22 @@ class CalcController {
 
     this._operation = [result, last];
 
+    this.setLastNumberToDisplay();
+
   }
 
   setLastNumberToDisplay(){
 
-    
+    let lastNumber;
+
+    for(let i = this._operation.length-1; i => 0; i--){
+      if(!this.isOperator(this._operation[i])){
+        lastNumber = this._operation[i];
+        break;
+      }
+    }
+
+    this.displayCalc = lastNumber
 
   }
 
@@ -76,6 +87,8 @@ class CalcController {
         console.log("outra coisa", value);
       } else {
         this.pushOperation(value);
+
+        this.setLastNumberToDisplay();
       }
     } else {
       if (this.isOperator(value)) {
